09c20519e916aa6c487dd6f7ecd0a6c96b0ec53a
mingfei <mingfei.net@gmail.com>
2017-07-05 10:40:12 +0800

    Update

--- src/main/java/demo/service/UserService.java
+++ src/main/java/demo/service/UserService.java
@@ -1,8 +1,10 @@
 package demo.service;
 
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
 import demo.mapper.UserMapper;
 import demo.model.User;
 import demo.util.MyBatisSession;
+import org.apache.ibatis.jdbc.SQL;
 import org.apache.ibatis.session.SqlSession;
 
 /**
@@ -18,6 +20,18 @@ public class UserService {
         }
     }
 
+    private static int updateUser() {
+        try (SqlSession sqlSession = MyBatisSession.getSqlSession(true)) {
+            return sqlSession.update("user.update", new User(1, "tester", "test"));
+        }
+    }
+
+    private static int deleteUser() {
+        try (SqlSession sqlSession = MyBatisSession.getSqlSession(true)) {
+            return sqlSession.delete("user.delete");
+        }
+    }
+
     private static int createUser() {
         try (SqlSession sqlSession = MyBatisSession.getSqlSession(true)) {
             UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
@@ -26,7 +40,9 @@ public class UserService {
     }
 
     public static void main(String[] args) {
-        System.out.println(createUserViaXml());
+//        System.out.println(createUserViaXml());
 //        System.out.println(createUser());
+//        System.out.println(updateUser());
+        System.out.println(deleteUser());
     }
 }
