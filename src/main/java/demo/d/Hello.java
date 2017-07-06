b9a8d8248b4bb111a7ab0faa589dcfb9e3cc7a59
mingfei <mingfei.net@gmail.com>
2017-07-06 11:38:18 +0800

    Update

---
+++ src/main/java/demo/d/Hello.java
@@ -0,0 +1,14 @@
+package demo.d;
+
+/**
+ * Created by mingfei.net@gmail.com
+ * 7/6/17 11:36
+ * https://github.com/thu/JavaEE_Framework_1702A/
+ */
+public class Hello {
+    private String name;
+
+    public void sayHello() {
+        System.out.println("Hello, " + name);
+    }
+}
---
+++ src/main/java/demo/d/Service.java
@@ -0,0 +1,14 @@
+package demo.d;
+
+/**
+ * Created by mingfei.net@gmail.com
+ * 7/6/17 11:37
+ * https://github.com/thu/JavaEE_Framework_1702A/
+ */
+public class Service {
+    public static void main(String[] args) {
+        /*
+            使用 Spring 方式，输出 “Hello, Tom”
+         */
+    }
+}
