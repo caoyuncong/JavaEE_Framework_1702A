f0e70b85519d568a616709f35faffe2e99ce5cea
mingfei <mingfei.net@gmail.com>
2017-07-11 09:14:05 +0800

    Update

--- sql/db_test.sql
+++ sql/db_test.sql
@@ -5,11 +5,11 @@ CREATE DATABASE db_test;
 DROP TABLE IF EXISTS db_test.user;
 CREATE TABLE db_test.user (
   id       INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255),
+  username VARCHAR(191) UNIQUE,
   password VARCHAR(255)
 );
 
+TRUNCATE TABLE db_test.user;
+
 SELECT *
 FROM db_test.user;
-
-TRUNCATE TABLE db_test.user;
